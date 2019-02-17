318d56ccfd439d3880d539e489812998458c8fdf
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2019-02-17 11:36:58 +0700

    Initial commit

---
+++ el-test/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  el-test
+//
+//  Created by Евгений Бижанов on 04/02/2019.
+//  Copyright © 2019 Евгений Бижанов. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ el-test/Resources/AppConfig.swift
@@ -0,0 +1,20 @@
+//
+//  AppConfig.swift
+//  el-test
+//
+//  Created by Евгений Бижанов on 04/02/2019.
+//  Copyright © 2019 Евгений Бижанов. All rights reserved.
+//
+
+struct AppConfig {
+    
+    
+    // MARK: - Authentication
+    
+    struct Authentication {
+        static let id = "6349018"
+        static let key = "hg792B7zrq7AOY73MZrv"
+        static let url = "https://oauth.vk.com/authorize"
+    }
+    
+}
